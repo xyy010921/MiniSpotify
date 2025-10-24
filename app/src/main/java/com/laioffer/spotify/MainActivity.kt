919e42564f5d916475e57926470258f146a163d0
Yiyang Xue <yxue30@syr.edu>
2025-10-24 16:00:28 -0400

    Added bottom navigation bar for the main activity

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,45 +1,213 @@
 package com.laioffer.spotify
 
+
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
-class MainActivity : ComponentActivity() {
+// customized extend AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    //private val TAG = "lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        //Log.d(TAG, "We are at onCreate()")
+//        setContent {
+//            SpotifyTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    AlbumCover()
+//                }
+//            }
+//        }
+        setContentView(R.layout.activity_main)
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AlbumCover() {
+    Column {
+        Box (modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                model = "https://images.unsplash.com/photo-1733866055327-762ba798ed48?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1750",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = "Still Fantasy",
+                modifier = Modifier
+                    .padding(start = 2.dp, bottom = 4.dp) // left is start, right is end
+                    .align(Alignment.BottomStart),
+                style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+                color = Color.White
+            )
+        }
+
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(top = 4.dp, start = 2.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray
+        )
+    }
 }
+
+@Composable
+fun ArtistCardBox() {
+    Box {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Composable
+fun ArtistCardColumn() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Composable
+fun ArtistCardRow() {
+    Row {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardBox() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardBox()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardRow() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardRow()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardColumn() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardColumn()
+        }
+    }
+}
+
+
+@Composable
+private fun Greeting(name: String) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+//        .padding(24.dp)
+        .background(Color.Yellow)
+        .padding(24.dp)
+
+    ) {
+        Text(text = "Hello,")
+        Text(text = name)
+    }
+}
+
+@Composable
+fun HelloContent() {
+//    var name: String = "" // state
+    // by: delegation
+    var name by remember { mutableStateOf("") } // state
+
+    Column {
+        if (name.isNotEmpty()) {
+            Text(
+                modifier = Modifier.padding(bottom = 8.dp),
+                text = "Hello! $name",
+                style = MaterialTheme.typography.body2
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            label = { Text(text = "name") }
+        )
+    }
+}
+
+@Composable
+fun HelloContentStateless(name: String, onNameChange: (String) -> Unit) {
+    Column {
+        if (name.isNotEmpty()) {
+            Text(
+                modifier = Modifier.padding(bottom = 8.dp),
+                text = "Hello! $name",
+                style = MaterialTheme.typography.body2
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text(text = "name") }
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewGreeting() {
     SpotifyTheme {
         Surface {
-            Greeting("Android")
+            HelloContent()
         }
     }
 }
+
+
+
+
